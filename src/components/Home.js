339ee035af2821c3c6be803b3523515604229d8e
BI TAH DIDIER TAH <didier.tah@mqashgroup.com>
2023-04-20 14:41:37 +0000

    continuos

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,6 +32,27 @@ function Home() {
       leftBtnText="Custom order"
       rightBtnText="Existing inventory"
       />
+      <Section 
+      title="Lowest Cost Solar Panels in America"
+      description="Money-back guarantee"
+      backgroundImg="solar-panel.jpg"
+      leftBtnText="Order now"
+      rightBtnText="Learn more"
+      />
+      <Section 
+      title="Solar for Nex Roofs"
+      description="Solar Roof Costs Less Than a New Roof Plus Solar Panels "
+      backgroundImg="solar-roof.jpg"
+      leftBtnText="Order now"
+      rightBtnText="Learn more"
+      />
+      <Section 
+      title="Accessories"
+      description=" "
+      backgroundImg="accessories.jpg"
+      leftBtnText="Shop now"
+     
+      />
     </Container>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import styled from "styled-components"
-function Section({title,description,leftBtnText,rightBtnText,backgroundImg}) {
-  
+import React from "react";
+import styled from "styled-components";
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
     <Wrap bgImage={backgroundImg}>
       <ItemText>
@@ -9,82 +14,70 @@ function Section({title,description,leftBtnText,rightBtnText,backgroundImg}) {
         <p>{description}</p>
       </ItemText>
       <Buttons>
-      <ButtonGroup>
-        <LeftButton>
-          {leftBtnText}
-        </LeftButton>
-        <RightButton>
-         {rightBtnText}
-        </RightButton>
-      </ButtonGroup>
-      <DownArrow src="/images/down-arrow.svg"/>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
-      
-      
     </Wrap>
-  )
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-
-width : 100vw;
-height : 100vh;
-background-size: cover;
-background-position:center;
-background-repeat:no-repeat;
-background-image: url('/images/model-s.jpg');
-display: flex;
-flex-direction: column;
-justify-content: space-between; //vertical
-align-items: center; //Horizontal
-background-image:${props=>`url("/images/${props.bgImage}")`}
-
-`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //Horizontal
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
 
 const ItemText = styled.div`
-padding-top:15vh;
+  padding-top: 15vh;
 
-text-align: center;
-`
+  text-align: center;
+`;
 const ButtonGroup = styled.div`
- display: flex;
- margin-bottom:30px;
- @media(max-width: 768px) {
-  flex-direction:column;
-}
-`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 const LeftButton = styled.div`
-background-color: rgba(23,26,32,0.8);
-height: 40px;
-width: 256px;
-color: white;
-display: flex;
-justify-content: center;
-align-items: center;
-border-radius: 100px;
-opacity: 0.85;
-text-transform: uppercase;
-font-size: 12px;
-cursor: pointer;
-margin: 8px;
-
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 const RightButton = styled(LeftButton)`
-background-color:white;
-color: black;
-opacity: 0.65;
-`
+  background-color: white;
+  color: black;
+  opacity: 0.65;
+`;
 const DownArrow = styled.img`
-margin-top:20px;
-height: 40px;
-cursor: pointer;
-overflow-x: hidden;
-animation: animateDown infinite 1.5s;
+  margin-top: 20px;
+  height: 40px;
+  cursor: pointer;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
 
-`
-
-const Buttons = styled.div`
-
-`
+const Buttons = styled.div``;
