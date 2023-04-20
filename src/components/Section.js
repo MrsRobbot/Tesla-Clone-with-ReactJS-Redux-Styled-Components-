948db4bb3c672b40f0efef1bd4891d9f23d6a42f
BI TAH DIDIER TAH <didier.tah@mqashgroup.com>
2023-04-20 12:51:01 +0000

    added images folders and contents

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,10 @@ import styled from "styled-components"
 function Section() {
   return (
     <Wrap>
-      Section
+      <ItemText>
+        <h1>MODEL S</h1>
+        <p>Order Online fir Touchless Delivery</p>
+      </ItemText>
     </Wrap>
   )
 }
@@ -14,11 +17,14 @@ const Wrap = styled.div`
 
 width : 100vw;
 height : 100vh;
-background : orange;
+background-size: cover;
+background-position:center;
+background-repeat:no-repeat;
+background-image: url('/images/model-s.jpg')
+`
 
+const ItemText = styled.div`
+padding-top:15vh;
 
-
-
-
-
+text-align: center;
 `
