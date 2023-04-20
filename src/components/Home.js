1d0d0fc33e3e361c087ab99a226963b24e4622ab
BI TAH DIDIER TAH <didier.tah@mqashgroup.com>
2023-04-20 14:20:10 +0000

    added other section

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,34 @@ import Section from './Section.js'
 function Home() {
   return (
     <Container>
-      <Section/>
+      <Section
+      title="Model S"
+      description="Order Online For Touchless Delivery"
+      backgroundImg="model-s.jpg"
+      leftBtnText="Custom order"
+      rightBtnText="Existing inventory"
+      />
+      <Section
+      title="Model Y "
+      description="Order Online For Touchless Delivery"
+      backgroundImg="model-y.jpg"
+      leftBtnText="Custom order"
+      rightBtnText="Existing inventory"
+      />
+      <Section
+      title="Model 3"
+      description="Order Online For Touchless Delivery"
+      backgroundImg="model-3.jpg"
+      leftBtnText="Custom order"
+      rightBtnText="Existing inventory"
+      />
+      <Section 
+      title="Model X"
+      description="Order Online For Touchless Delivery"
+      backgroundImg="model-x.jpg"
+      leftBtnText="Custom order"
+      rightBtnText="Existing inventory"
+      />
     </Container>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import styled from "styled-components"
-function Section() {
+function Section({title,description,leftBtnText,rightBtnText,backgroundImg}) {
+  
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>MODEL S</h1>
-        <p>Order Online fir Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
+      <Buttons>
+      <ButtonGroup>
+        <LeftButton>
+          {leftBtnText}
+        </LeftButton>
+        <RightButton>
+         {rightBtnText}
+        </RightButton>
+      </ButtonGroup>
+      <DownArrow src="/images/down-arrow.svg"/>
+      </Buttons>
+      
+      
     </Wrap>
   )
 }
@@ -20,11 +34,57 @@ height : 100vh;
 background-size: cover;
 background-position:center;
 background-repeat:no-repeat;
-background-image: url('/images/model-s.jpg')
+background-image: url('/images/model-s.jpg');
+display: flex;
+flex-direction: column;
+justify-content: space-between; //vertical
+align-items: center; //Horizontal
+background-image:${props=>`url("/images/${props.bgImage}")`}
+
 `
 
 const ItemText = styled.div`
 padding-top:15vh;
 
 text-align: center;
+`
+const ButtonGroup = styled.div`
+ display: flex;
+ margin-bottom:30px;
+ @media(max-width: 768px) {
+  flex-direction:column;
+}
+`
+const LeftButton = styled.div`
+background-color: rgba(23,26,32,0.8);
+height: 40px;
+width: 256px;
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 100px;
+opacity: 0.85;
+text-transform: uppercase;
+font-size: 12px;
+cursor: pointer;
+margin: 8px;
+
+`
+const RightButton = styled(LeftButton)`
+background-color:white;
+color: black;
+opacity: 0.65;
+`
+const DownArrow = styled.img`
+margin-top:20px;
+height: 40px;
+cursor: pointer;
+overflow-x: hidden;
+animation: animateDown infinite 1.5s;
+
+`
+
+const Buttons = styled.div`
+
 `
